51ba274a04e62801cc928cc0e0001c2cef387b75
Andrey <lagunmelman@gmail.com>
2024-12-09 15:49:58 +0300

    Первый запуск приложения с помощью uvicorn

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=8000,
+        reload=True,
+    )
