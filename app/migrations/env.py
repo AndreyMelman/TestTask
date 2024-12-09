d7157e6e0bdc9b5603bca81cb756f9e759bb4979
Andrey <lagunmelman@gmail.com>
2024-12-10 00:19:37 +0300

    обновление полей

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -7,8 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-
-from core import Base
+from models import Author # noqa
+from core.db import Base
 from core.config import settings
 
 # this is the Alembic Config object, which provides
