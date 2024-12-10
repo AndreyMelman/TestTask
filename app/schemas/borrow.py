51a9f744c6cb723a317cba5badde60fdfcdd1caa
Andrey <lagunmelman@gmail.com>
2024-12-10 22:24:53 +0300

    Правка схемы выдачи

--- app/schemas/borrow.py
+++ app/schemas/borrow.py
@@ -6,18 +6,20 @@ from pydantic import BaseModel, ConfigDict, Field
 
 class BorrowBase(BaseModel):
     reader_name: Annotated[str, Field(max_length=32)]
-    date_of_issue: datetime
+    date_of_issue: datetime = Field(default=datetime.now())
     date_return: datetime | None
+    book_id: int
 
 
 class BorrowCreate(BorrowBase):
-    pass
+    date_return: None
 
-class BorrowUpdate(BorrowBase):
-    pass
+
+class BorrowUpdate(BaseModel):
+    date_return: datetime = Field(default=datetime.now())
+
 
 class Borrow(BorrowBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
-    book_id: int
